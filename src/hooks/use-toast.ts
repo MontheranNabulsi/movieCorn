08c4b021a99a4be703c8c2f963dcaf78c75be054
MontheranNabulsi <129999292+MontheranNabulsi@users.noreply.github.com>
2025-10-06 20:58:49 +0300

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,10 +1,11 @@
+//init
 import * as React from "react"
 
 import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
-//init
+
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
 
